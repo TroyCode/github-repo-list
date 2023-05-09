f7fe60c1a48a1a54af744c07ac2479f490c08168
Troy Wu <pightmp@gmail.com>
2023-05-09 18:06:09 +0800

    fix gh-page issue

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/github-repo-list/',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    host: 'https://troycode.github.io/'
   }
 })
