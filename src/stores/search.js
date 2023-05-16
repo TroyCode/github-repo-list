882adcc1389b122f8104fe10d0486050ad9a6ffb
Troy Wu <pightmp@gmail.com>
2023-05-17 00:22:16 +0900

    add termination condition and loading status

--- src/stores/search.js
+++ src/stores/search.js
@@ -7,24 +7,31 @@ export const useSearchStore = defineStore('search', () => {
   const data = ref([])
   const page = ref(1)
   const loading = ref(false)
+  const finished = ref(false)
 
   function modifyKeyword(k) {
     keyword.value = k
     page.value = 1
     data.value = []
+    finished.value = false
   }
   async function fetchRepos() {
-    loading.value = true
-    try {
-      const result = await axios.get(
-        `https://api.github.com/users/${keyword.value}/repos?per_page=6&page=${page.value}`
-      )
-      data.value = [...data.value, ...result.data]
-      page.value += 1
-      loading.value = false
-    } catch (error) {
-      console.log(error)
-      loading.value = false
+    if (!loading.value && !finished.value) {
+      try {
+        loading.value = true
+        const result = await axios.get(
+          `https://api.github.com/users/${keyword.value}/repos?per_page=6&page=${page.value}`
+        )
+        data.value = [...data.value, ...result.data]
+        page.value += 1
+        if (data.value.length === 0) {
+          finished.value = true
+        }
+        loading.value = false
+      } catch (error) {
+        console.log(error)
+        loading.value = false
+      }
     }
   }
 
